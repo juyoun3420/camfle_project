a50592891ebbd8a1ed888b16db4939aaa21d62aa
Bokdolll <hope105412@gmail.com>
2023-11-22 02:04:06 +0000

    11주차 그룹실습과제2 완료

--- database.py
+++ database.py
@@ -59,3 +59,16 @@ class DBhandler:
             if value['id'] == id_ and value['pw'] == pw_:
                 return True
             return False
+    def get_items(self):
+        items=self.db.child("item").get().val()
+        return items
+    
+    def get_item_byname(self, name):
+        items = self.db.child("item").get()
+        target_value=""
+        print("###########",name)
+        for res in items.each():
+            key_value = res.key()
+            if key_value == name:
+                target_value=res.val()
+                return target_value
