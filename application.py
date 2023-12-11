ebbcdfffc7f45bc26cb2bfdf29f555fff7ed0d6d
haein <uuuhaeiii@ewhain.net>
2023-12-11 05:23:42 +0000

    signup

--- application.py
+++ application.py
@@ -33,11 +33,26 @@ def login_user():
         flash("Wrong ID or PW!")
         return render_template("login.html")
 
+
 @application.route("/logout")
 def logout_user():
     session.clear()
     return redirect(url_for('view_list'))
 
+@application.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw=request.form['pw']
+    pw_hash=hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data, pw_hash):
+        return render_template("login.html")
+    else :
+        flash("user id already exist!")
+        return render_template("signup.html")
 
 @application.route("/list")
 def view_list():
